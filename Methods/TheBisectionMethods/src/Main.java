fd762896e8c5e7763654c12c20a1057a542813ac
Berkay Ercan <115825102+berkayercan3@users.noreply.github.com>
2025-04-12 02:16:02 +0300

    TheBisectionMethod

--- Methods/TheBisectionMethods/src/Main.java
+++ Methods/TheBisectionMethods/src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.lang.Math;
-
+//bisectionmethod
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
